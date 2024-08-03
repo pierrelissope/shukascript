a6e35981bcaeb30fd63e6980203c0f3732b5d410
Pierre <pierrelissope@gmail.com>
2024-08-03 14:33:57 +0200

    fix(expression): fixed string concatenate

--- source/evaluate_expression.cpp
+++ source/evaluate_expression.cpp
@@ -7,14 +7,13 @@
 variant_t Expression::evaluate(std::map<std::string, variant_t> variables, Visitor *v)
 {    
     if (!op.empty() && left && right) {
-
         auto left_val = left->evaluate(variables, v);
         auto right_val = right->evaluate(variables, v);
-
+    
         variant_t res = std::visit([this](auto&& l, auto&& r) -> variant_t {
             using LType = std::decay_t<decltype(l)>;
             using RType = std::decay_t<decltype(r)>;
-
+    
             if constexpr (std::is_arithmetic_v<LType> && std::is_arithmetic_v<RType>) {
                 if (this->op == "/") return l / r;
                 if (this->op == "*") return l * r;
@@ -27,16 +26,15 @@ variant_t Expression::evaluate(std::map<std::string, variant_t> variables, Visit
                 if (this->op == "==") return l == r;
                 if (this->op == "&&") return l && r;
                 if (this->op == "||") return l || r;
+            } else if constexpr (std::is_same_v<LType, std::string> && std::is_same_v<RType, std::string>) {
+                if (this->op == "+") return l + r;
             } else {
-
-                if (std::is_same_v<LType, std::string> && std::is_same_v<RType, std::string>) {
-                    if (this->op == "+") return l + r;
-                } else {
-                    throw std::runtime_error("Invalid operation on non-arithmetic types");
-                }
+                throw std::runtime_error("Invalid operation on non-arithmetic types");
             }
+    
             throw std::runtime_error("Unhandled case in expression evaluation");
         }, left_val, right_val);
+    
         return res;
     } else {
         throw std::runtime_error("Invalid operation on arithmetic types");
